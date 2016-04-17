57004df4301236c99af10ef175c6a074102699f0
Bart Ariens <bart.ariens@divimove.com>
2016-04-17 17:21:29 +0200

    adds very basic user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :require_login
+
+
+  private
+  def not_authenticated
+    redirect_to login_path, alert: "Please login first"
+  end
+
+  def is_admin?
+    current_user && current_user.admin
+  end
+
+  def must_be_admin
+    unless is_admin?
+      redirect_to login_path, alert: "Administrators only"
+    end
+  end
 end
